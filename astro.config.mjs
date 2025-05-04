5c8926232914f2a50aaad5f3724d0ba92713db2a
HoangAnh <vhgah98@gmail.com>
2025-05-04 14:26:22 +0700

    add vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vue from '@astrojs/vue';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [vue()],
+    devToolbar: {
+        enabled: false,
+    }
+});
---
+++ src/components/Welcome.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="welcome">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+<script setup lang="ts">
+const msg = 'Welcome to Your Vue.js App'
+</script>
